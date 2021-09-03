c442a359be540adde3b9a1ecde3883c2cae88b23
sudharaD <sudharad7@gmail.com>
2021-09-03 12:28:08 +0530

    all sounds added

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,62 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int number) {            // Inputs arguments to the functions
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  playSound(1);           // Inputs arguments to the functions
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('Click Me'),
+              ),
+            ],
           ),
         ),
       ),
