fc4b73a1b102411560e803a28b0b26c932b81d2b
sudharaD <sudharad7@gmail.com>
2021-09-04 00:19:49 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,44 @@
-import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int number) {
+    // Inputs arguments to the functions
+    // final player = AudioPlayer();
+    AudioPlayer player = AudioPlayer();
 
-  void playSound(int number) {            // Inputs arguments to the functions
-    final player = AudioCache();
     player.play('note$number.wav');
+  }
+
+  Expanded buildKey({Color color: Colors.white, int soundNumber: 1}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                onPressed: () {
-                  playSound(1);           // Inputs arguments to the functions
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(2);
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(3);
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(4);
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(5);
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(6);
-                },
-                child: Text('Click Me'),
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(7);
-                },
-                child: Text('Click Me'),
-              ),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.blue, soundNumber: 5),
+              buildKey(color: Colors.indigo, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
             ],
           ),
         ),
